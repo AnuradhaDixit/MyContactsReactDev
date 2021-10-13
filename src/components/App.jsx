84263c2dfb7d7abcb6dd1858ef61a6131b55b2b8
Anuradha Dixit <82029747+AnuradhaDixit@users.noreply.github.com>
2021-10-14 01:59:20 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
+// import Avatar from "./Avatar";
+
+function createCard(contacts) {
+  return (
+    <Card
+      id={contacts.id}
+      key={contacts.id}
+      name={contacts.name}
+      img={contacts.imgURL}
+      tel={contacts.phone}
+      email={contacts.email}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      {contacts.map(createCard)}
+
+      {/* < img="Avatarhttps://media4.picsearch.com/is?YLIh0T-7cEAG9EPmB2g2vxg-qMqk7iphS44fBpBfLGg&height=209"/> */}
+      {/* <Card
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
+      />
+      <Card
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        tel={contacts[2].phone}
+        email={contacts[2].email}
+      /> */}
+    </div>
+  );
+}
+
+export default App;
